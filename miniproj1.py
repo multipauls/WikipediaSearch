234af45054c48e50c509d0edffa17a30d60bdcad
Mona <shubhangi.dutta@research.iiit.ac.in>
2020-08-28 16:35:18 +0530

    got back

--- miniproj1.py
+++ miniproj1.py
@@ -11,9 +11,19 @@ titles=defaultdict(str)
 docCount=0
 stemmap=defaultdict(lambda:"")
 def stemming(word):
-	if stemmap[word]=="":
-		stemmap[word]=stemmer.stem(word)
+    if stemmap[word]=="":
+        stemmap[word]=stemmer.stem(word)
+    return stemmer.stem(word)
 def parsetext(text):
+    newtext =""
+    text = re.sub(u'=References==[^=]*=','=',text)
+    text = re.sub(u'=External links==[^=]*=','=',text)
+    text = re.sub(u'[^a-zA-Z0-9 ]+',' ',text)
+    text = re.sub(u'[ ]+',' ',text)
+    text = re.split(" ", text)
+    for i in text:
+        newtext+=(stemming(i)+" ")
+    print(newtext)
 
 class WikiHandler( xml.sax.ContentHandler):
     
@@ -30,12 +40,12 @@ class WikiHandler( xml.sax.ContentHandler):
 
     
     def startElement(self,tag,attr):
-        global no_of_docs
+        global docCount
         if(tag=="id" and self.page==0):
             self.page=1
             self.id=1
             self.bufid=""
-            no_of_docs+=1
+            docCount+=1
         elif(tag=="title"):
             self.title=1
             self.buftitle=""
@@ -46,7 +56,7 @@ class WikiHandler( xml.sax.ContentHandler):
     def characters(self,data):
         if (self.id==1 and self.page==1):
             self.bufid += data
-            title_dict[int(self.bufid)]=self.buftitle
+            titles[int(self.bufid)]=self.buftitle
         elif(self.title==1 ):
             self.buftitle += data
         elif(self.text==1):
@@ -66,14 +76,14 @@ class WikiHandler( xml.sax.ContentHandler):
         if(tag=="text"):
             self.text=0
             parsetext(self.buftext)
-	            
+                
 if __name__ == "__main__":                                            #main
     start = timeit.default_timer()
     parser = xml.sax.make_parser()
-  	parser.setFeature(xml.sax.handler.feature_namespaces, 0)
-   	Handler = WikiHandler()
-   	parser.setContentHandler( Handler )
+    parser.setFeature(xml.sax.handler.feature_namespaces, 0)
+    Handler = WikiHandler()
+    parser.setContentHandler( Handler )
    
-   	parser.parse("wiki_text.txt")
+    parser.parse("wiki_text.txt")
     stop = timeit.default_timer()
     print (stop - start)
